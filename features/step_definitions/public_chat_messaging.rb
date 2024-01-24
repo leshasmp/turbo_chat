eafd749f26c7107716569f9186295f5fd4505567
leshasmp <leshasmp1989@mail.ru>
2024-01-24 20:17:47 +0500

    Translate language in cucumber tests

--- features/step_definitions/public_chat_messaging.rb
+++ features/step_definitions/public_chat_messaging.rb
@@ -1,35 +1,35 @@
 # frozen_string_literal: true
 
-Given('Открываю главную страницу') do
+Дано('Открываю главную страницу') do
   create(:user, email: 'example@example.com', password: 'password')
   create(:user)
   visit root_path
 end
 
-When('Ввожу email и password') do
+Когда('Ввожу email и password') do
   fill_in 'user_email', with: 'example@example.com'
   fill_in 'user_password', with: 'password'
 end
 
-And("Нажимаю 'Log in'") do
+И("Нажимаю 'Log in'") do
   click_link_or_button 'Log in'
 end
 
-When('Переходим в общий чат') do
+Когда('Переходим в общий чат') do
   common_room = Room.where(is_common: true).first
   expect(page).to have_current_path(room_path(common_room), ignore_query: true)
   expect(page).to have_content('Signed in successfully.')
 end
 
-And('Ввожу сообщение в поле ввода чата') do
+И('Ввожу сообщение в поле ввода чата') do
   fill_in 'message_content', with: 'Моё новое сообщение'
 end
 
-And("Нажимает кнопку 'Send'") do
+И("Нажимает кнопку 'Send'") do
   click_link_or_button 'Send'
 end
 
-Then('Происходит отравка сообщения в общий чат') do
+Тогда('Происходит отравка сообщения в общий чат') do
   visit root_path
   within('.messages') do
     expect(page).to have_content('Моё новое сообщение')
